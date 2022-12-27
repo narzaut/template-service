e5d2b70c157e4116acc8824eeee186e8be98cdd1
Ignacio Arzaut <63589213+narzaut@users.noreply.github.com>
2022-12-27 20:04:34 -0300

    Update index.js

--- src/services/firebase/index.js
+++ src/services/firebase/index.js
@@ -1,39 +1,37 @@
 const admin = require('firebase-admin');
-
 const logger = requireFromRoot('config/logger');
+const firebaseCert = requireFromRoot('config/certs/simple-comercio-firebase-admin.json');
 
-const firebaseService = module.exports;
-firebaseService.connect = async () => {
+const firebaseService = {
+  connect: async () => {
     try {
-        const firebaseCert = requireFromRoot(
-            'config/certs/simple-comercio-firebase-admin.json'
-        );
-        admin.initializeApp({
-            credential: admin.credential.cert(firebaseCert),
-            databaseURL: 'https://simple-comercio.firebaseio.com'
-        });
+      admin.initializeApp({
+        credential: admin.credential.cert(firebaseCert),
+        databaseURL: 'https://simple-comercio.firebaseio.com'
+      });
     } catch (err) {
-        logger.debug(
-            'There was an error attempting to authenticate firebase service through credentials json'
-        );
+      logger.debug(
+        'There was an error attempting to authenticate firebase service through credentials json'
+      );
     }
+  },
+  setUserClaims: async (uid, data) => {
+    try {
+      const response = await admin.auth().setCustomUserClaims(uid, data);
+      return response;
+    } catch (err) {
+      logger.debug(err);
+      return err;
+    }
+  },
+  verifyToken: async (token) => {
+    try {
+      const decodedToken = await admin.auth().verifyIdToken(token);
+      return { decodedToken };
+    } catch (err) {
+      return { err };
+    }
+  }
 };
 
-firebaseService.setUserClaims = async (uid, data) => {
-    try {
-        const response = await admin.auth().setCustomUserClaims(uid, data);
-        return response;
-    } catch (err) {
-        logger.debug(err);
-        return err;
-    }
-};
-
-firebaseService.verifyToken = async (token) => {
-    try {
-        const decodedToken = await admin.auth().verifyIdToken(token);
-        return { decodedToken };
-    } catch (err) {
-        return { err };
-    }
-};
+module.exports = firebaseService;
